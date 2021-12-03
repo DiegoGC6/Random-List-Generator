eceb5b105a0e8575d4c0a1430634ac2fe64ec483
DiegoG <80786405+Skapebreak@users.noreply.github.com>
2021-12-03 00:07:15 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+function getList () {
+    var list = Array(10000).fill(0).map((e,i)=>i+1)
+    shuffleArray(list)
+    console.log(list)
+
+    var txtOutput = document.getElementById("txtOutput");
+    txtOutput.value = list
+}
